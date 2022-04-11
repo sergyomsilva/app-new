aa6dad8bd424be27a6e22df976de6541e8992f1b
Sergyo Moreira <sergyo.moreira@basis.com.br>
2022-04-11 13:24:50 -0300

    Adicionado commando para todo dia atualizar as notícias!!

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('news:get')->daily();
+        $schedule->command('queue:restart')->daily();
     }
 
     /**
